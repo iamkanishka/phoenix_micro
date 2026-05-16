d6fc9ac74da90359f06a5934762dbdaf1db21f19
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2026-05-16 11:16:48 +0530

    .formatter.exs

---
+++ .formatter.exs
@@ -0,0 +1,14 @@
+[
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+
+  locals_without_parens: [
+    # Consumer DSL macros
+    topic: 1,
+    concurrency: 1,
+    retry: 1,
+    middleware: 1,
+    dead_letter_topic: 1,
+    transport: 1,
+    queue_group: 1
+  ]
+]
